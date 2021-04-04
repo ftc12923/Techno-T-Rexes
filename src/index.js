5c1e874cee201b9cc413855bf4eae5eb989b5e95
prodSatvikK <rachitipidudraksha@gmail.com>
2021-04-04 19:52:44 -0400

    wrapped in router

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ import { HashRouter as Router} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
