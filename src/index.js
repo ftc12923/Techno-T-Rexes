adeffef6b30f0ce2eb0899da2fe120afa1bf14d7
prodSatvikK <rachitipidudraksha@gmail.com>
2021-04-04 19:49:22 -0400

    added hashrouter

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { HashRouter as Router} from 'react-router-dom';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
